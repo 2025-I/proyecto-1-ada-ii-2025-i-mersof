0d9c6f2b1a5170177860944edf21c23fabf8c6f9
sfcg2 <castillo.sofia@correounivalle.edu.co>
2025-05-03 18:07:47 -0500

    Conexion de los nodos (reglas de supervision encontradas)

--- src/ejercicio2.py
+++ src/ejercicio2.py
@@ -1,5 +1,4 @@
 def read_file():
-
     file2 = "file2.txt"
 
     with open(file2, 'r') as f:
@@ -27,14 +26,35 @@ def read_file():
 
         # en la linea de calificaciones separa y convierte cada caracter en int con la funcion map
 
-        max_suma(n, m, grafo, calification)
+        max_sumaVoraz(n, m, grafo, calification)
 
         # Se llama la funcion pasandole los datos del probloma para
         # resolver la maximizacion de las calificaciones
 
 
-def max_suma(n, m, grafo, calification):
+def max_sumaVoraz(n, m, grafo, calification):
+    rules = {i: int(0) for i in range(len(grafo))}
+    i = 0
+    nodo_actual = 0
+    while i < len(rules) - 1:
+        for index, rule in enumerate(grafo[nodo_actual]):
+            if rule > 0:
+                employable_supervised = grafo[nodo_actual].index(rule)
+                rules[nodo_actual] = employable_supervised
 
+        nodo_actual += 1
+        i += 1
+    print(rules)
+
+    maxima_calification = max(calification)
+    print(maxima_calification)
+
+
+def max_sumaDinamica(n, m, grafo, calification):
+    pass
+
+
+def max_sumaFuerzaBruta(n, m, grafo, calification):
     pass
 
 
