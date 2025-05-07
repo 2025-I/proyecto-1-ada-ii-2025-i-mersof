6abcb851032741b3e5652f66eed6964b63213aac
sfcg2 <castillo.sofia@correounivalle.edu.co>
2025-05-06 20:25:49 -0500

    Algoritmo Dinámico con comentarios

--- src/ejercicio2.py
+++ src/ejercicio2.py
@@ -18,7 +18,7 @@ def read_file(funcion):
         m = int(lines[i])  # numero de empleados
         i += 1
 
-        grafo = []          # Genera la matriz de las reglas de supervision
+        grafo = []  # Genera la matriz de las reglas de supervision
         for _ in range(m):
             grafo.append(lines[i].split())
             i += 1
@@ -35,6 +35,7 @@ def read_file(funcion):
 
 
 def arbol_reglasSupervicion(m, grafo, calificaciones, funcion):
+    # Genera un diccionario donde cada llave es un supervisor y una lista de values que son los subordinados
     reglas = {i: [] for i in range(m)}
     tiene_supervisor = [False] * m
 
@@ -44,19 +45,20 @@ def arbol_reglasSupervicion(m, grafo, calificaciones, funcion):
                 reglas[supervisor].append(supervisado)
                 tiene_supervisor[supervisado] = True
 
-    # Detectar raíz (el que no tiene padre) para el algoritmo dinamico
+    # Detectar raíz (el empleado que no tiene supervisor) para el algoritmo dinamico
 
     raiz = next(i for i, tiene in enumerate(tiene_supervisor) if not tiene)
 
     if funcion == "voraz":
         return max_sumaVoraz(m, reglas, calificaciones)
     elif funcion == "dinamica":
-        return max_sumaDinamica(raiz)
+        return max_sumaDinamica(m, raiz, reglas, calificaciones)
     elif funcion == "bruta":
         return max_sumaFuerzaBruta()
 
 
 def max_sumaVoraz(m, reglas, calificaciones):
+
     maxima_calificacion = max(calificaciones)  # Escogemos el valor maximo de la lista calificaciones
     index_maximo = calificaciones.index(
         maxima_calificacion)  # Obtenemos su indice del empelado con el valor maximo para saber en que posicion se ubica en invitados
@@ -105,14 +107,53 @@ def max_sumaVoraz(m, reglas, calificaciones):
     invitados.append(suma_maxima)
     return invitados
 
+#Enfoque TOP-DOWN -> Guarda resultados, para no volverlos a calcular
 
-def max_sumaDinamica(n, m, grafo, calification):
-    pass
+dp = {} #Diccionario donde se guardan los resultados optimos de los subproblemas
+#subprolemas:
+def max_sumaDinamica(m, raiz, reglas, calificaciones):
 
+    #Utilizamos esta funcion para recorrer desde abajo hacia arriba
+    def recorrido(nodo):
+
+        no_invitado_empleado = 0
+
+        # Se verifica si el empleado no esta en los subordinados de empleado : 0...n
+        # lo que significa, que verifica si no se supervisa a si mismo
+        invitado_empleado = calificaciones[nodo] if nodo not in reglas[nodo] else 0
+
+        # recorremos todos los subordinados de cada empleado
+        for subordinados in reglas[nodo]:
+            recorrido(subordinados)
+            no_invitado_empleado += max(dp[subordinados])
+            invitado_empleado += dp[subordinados][0]
+
+        dp[nodo] = (no_invitado_empleado, invitado_empleado)
+
+    recorrido(raiz)
+    invitados = [0] * m
+
+    def construir_solcionOptima(nodo, tomar_supervisor):
+
+        no_invitado, invitado = dp[nodo]
+
+        invitar = not tomar_supervisor and invitado > no_invitado
+
+        invitados[nodo] = 1 if invitar else 0
+
+        for subordinado in reglas[nodo]:
+            construir_solcionOptima(subordinado, invitar)
+
+    construir_solcionOptima(raiz, False)
+
+    suma_maxima = sum(calificaciones[i] for i in range(m) if invitados[i] == 1)
+
+    invitados.append(suma_maxima)
+    return invitados
 
 def max_sumaFuerzaBruta(n, m, grafo, calification):
     pass
 
 
 if __name__ == "__main__":
-    read_file()
+    read_file("voraz")
